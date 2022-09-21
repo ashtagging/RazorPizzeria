93b5441fd0040710479a35f32ba911be703dc473
Ashley Littlewood <ashley.littlewood22@gmail.com>
2022-09-21 20:54:38 +0100

    Added Additional Pre made Pizza Informationa and dispaly on ViewModel

--- RazorPizzeria/Pages/Pizza.cshtml.cs
+++ RazorPizzeria/Pages/Pizza.cshtml.cs
@@ -1,12 +1,80 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzeria.Models;
 
 namespace RazorPizzeria.Pages
 {
     public class PizzaModel : PageModel
     {
+        // Imported Model 
+        public List<PizzasModel> PizzaDB = new List<PizzasModel>()
+        {
+            new PizzasModel(){
+                ImageTitle="Margerita", 
+                PizzaName="Margerita", 
+                BasePrice=2, 
+                TomatoSauce=true, 
+                Cheese=true, 
+                FinalPrice=4 
+            },
+            new PizzasModel(){
+                ImageTitle="Bolognese",
+                PizzaName="Bolognese",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                FinalPrice=4
+            },
+            new PizzasModel(){
+                ImageTitle="Carbonara",
+                PizzaName="Carbonara",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                FinalPrice=4
+            },
+            new PizzasModel(){
+                ImageTitle="MeatFeast",
+                PizzaName="MeatFeast",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                FinalPrice=4
+            },
+            new PizzasModel(){
+                ImageTitle="Mushroom",
+                PizzaName="Mushroom",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                FinalPrice=4
+            },
+            new PizzasModel(){
+                ImageTitle="Pepperoni",
+                PizzaName="Pepperoni",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                FinalPrice=4
+            },new PizzasModel(){
+                ImageTitle="Seafood",
+                PizzaName="Seafood",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                FinalPrice=4
+            }, new PizzasModel() {
+                ImageTitle="Vegetarian",
+                PizzaName="Vegetarian",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                FinalPrice=4
+            }
+        };
         public void OnGet()
         {
+
         }
     }
 }
